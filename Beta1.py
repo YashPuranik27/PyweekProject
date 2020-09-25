19bbcb4a3914cac9b77416cf2118dcf0eb6fc2b0
rozzledred <56243125+rozzledred@users.noreply.github.com>
2020-09-24 23:15:35 -0400

    Add files via upload

--- Beta1.py
+++ Beta1.py
@@ -1,115 +1,95 @@
 import pygame
 import random
 import sys
-
-pygame.init()
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
 
 WIDTH = 1000
 HEIGHT = 1000
-
-RED = (255,0,0)
-ORANGE = (255,165,0)
-YELLOW = (255,255,0)
-GREEN = (0,128,0)
-BACKGROUND_COLOR = (0,0,255)
-
-player_size = 50
-player_pos = [WIDTH/2, HEIGHT-2*player_size]
-
-enemy_size = 50
-enemy_pos = [random.randint(0,WIDTH-enemy_size), 0]
-enemy_list = [enemy_pos]
-
-tower_size = 50
-tower_pos = (400,300)
+RED = (255, 0, 0)
+ORANGE = (255, 165, 0)
+YELLOW = (255, 255, 0)
+GREEN = (0, 128, 0)
+WHITE = (255, 255, 255)
+BACKGROUND_COLOR = (0, 0, 255)
 
 
-SPEED = 10
+class Player(pygame.sprite.Sprite):
+    def __init__(self, width, height):
+        super(Player, self).__init__()
+        self.x = 0
+        self.y = 0
+        self.image = pygame.image.load('ship.png').convert_alpha()
+        self.width = pygame.Surface.get_width(self.image)*0.5
+        self.height = pygame.Surface.get_height(self.image)*0.5
+        self.rect = self.image.get_rect()
 
+    def update(self):
+        key = pygame.key.get_pressed()
+        if key[K_UP]:
+            if self.y-HEIGHT/100 >= 0:
+                self.y -= HEIGHT/100
+        if key[K_DOWN]:
+            if self.y+HEIGHT/100 <= HEIGHT-self.height:
+                self.y += HEIGHT/100
+        if key[K_LEFT]:
+            if self.x-WIDTH/100 >= 0:
+                self.x -= WIDTH/100
+        if key[K_RIGHT]:
+            if self.x+WIDTH/100 <= WIDTH-self.width:
+                self.x += WIDTH/100
+
+    def draw(self, surface):
+        surface.blit(self.image, (self.x, self.y))
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, width, height):
+        super(Enemy, self).__init__()
+        self.counter = 0
+        self.x = 0
+        self.y = 0
+        self.surf = pygame.Surface((width, height))
+        self.rect = self.surf.get_rect()
+
+    def update(self):
+        self.x += 5
+
+    def draw(self, surface):
+        surface.blit(self.surf, (self.x, self.y))
+
+
+pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-
+player = Player(50, 50)
+enemy = Enemy(50, 50)
 game_over = False
-
 score = 0
-
 clock = pygame.time.Clock()
-
 myFont = pygame.font.SysFont("monospace", 35)
-
-def set_level(score, SPEED):
-	if score < 20:
-		SPEED = 5
-	elif score < 40:
-		SPEED = 8
-	elif score < 60:
-		SPEED = 12
-	else:
-		SPEED = 15
-	return SPEED
-	# SPEED = score/5 + 1
-
-def draw_tower():
-	pygame.draw.rect(tower_pos)
-
-
-def draw_enemies(enemy_list):
-	for enemy_pos in enemy_list:
-		pygame.draw.rect(screen, ORANGE, (enemy_pos[0], enemy_pos[1], enemy_size, enemy_size))
-
-def collision_check(enemy_list, player_pos):
-	for enemy_pos in enemy_list:
-		if detect_collision(enemy_pos, player_pos):
-			return True
-	return False
-
-def detect_collision(player_pos, enemy_pos):
-	p_x = player_pos[0]
-	p_y = player_pos[1]
-
-	e_x = enemy_pos[0]
-	e_y = enemy_pos[1]
-
-	if (e_x >= p_x and e_x < (p_x + player_size)) or (p_x >= e_x and p_x < (e_x+enemy_size)):
-		if (e_y >= p_y and e_y < (p_y + player_size)) or (p_y >= e_y and p_y < (e_y+enemy_size)):
-			return True
-	return False
+BG = pygame.image.load("water.png").convert_alpha()
 
 while not game_over:
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                game_over = True
+        elif event.type == QUIT:
+            game_over = True
 
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			sys.exit()
-
-		if event.type == pygame.KEYDOWN:
-
-			x = player_pos[0]
-			y = player_pos[1]
-
-			if event.key == pygame.K_LEFT:
-				x -= player_size
-			elif event.key == pygame.K_RIGHT:
-				x += player_size
-			elif event.key == pygame.K_UP:
-				y -= player_size
-			elif event.key == pygame.K_DOWN:
-				y += player_size
-			player_pos = [x,y]
-
-	screen.fill(BACKGROUND_COLOR)
-
-
-	text = "Score:" + str(score)
-	label = myFont.render(text, 1, YELLOW)
-	screen.blit(label, (WIDTH-500, HEIGHT-40))
-
-	if collision_check(enemy_list, player_pos):
-		game_over = True
-		break
-
-	draw_enemies(enemy_list)
-
-	pygame.draw.rect(screen, RED, (player_pos[0], player_pos[1], player_size, player_size))
-
-	clock.tick(30)
-
-	pygame.display.update()
+    screen.fill(BACKGROUND_COLOR)
+    screen.blit(BG, (0, 0))
+    player.draw(screen)
+    enemy.draw(screen)
+    pressed_keys = pygame.key.get_pressed()
+    player.update()
+    enemy.update()
+    clock.tick(60)
+    pygame.display.update()
